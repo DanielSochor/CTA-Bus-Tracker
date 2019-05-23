8e76769aa2885721d78bba494b03f7c1dc117528
Daniel Sochor <47047939+DanielSochor@users.noreply.github.com>
2019-05-23 12:07:44 -0500

    Add files via upload
    Updated Error Messages

--- assets/app.js
+++ assets/app.js
@@ -10,20 +10,28 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         }).done(function (response) {
-            console.log(response);
-            var comingBuses = response['bustime-response'].prd.length;
-            console.log('There are ' + comingBuses + ' upcoming buses! They are:')
-            for (var i = 0; i < comingBuses; i++) {
-                console.log("Bus " + response['bustime-response'].prd[i].rt + " will arrive in " + response['bustime-response'].prd[i].prdctdn + " minutes");
-                var result = "Bus " + response['bustime-response'].prd[i].rt + " will arrive in " + response['bustime-response'].prd[i].prdctdn + " minutes";
-                $('#results').append('<p>' + result + '</p>');
-                //console.log('Concert ' + (i + 1) + ' is on ' + moment(response.data[i].datetime).format('MMMM Do YYYY') + ' at the ' + response.data[i].venue.name + ' in ' + response.data[i].venue.city + ', ' + response.data[i].venue.country);
-            }
+            results(response);
         }).fail(function () {
             $('#results').append('<p>' + 'No reults found!' + '</p>');
         });
     });
 });
+
+function results(response) {
+    console.log(response);
+    if (response['bustime-response'].error[0].msg == "No service scheduled" && response['bustime-response'].error[0].msg == "No arrival times") {
+        $('#results').append('<p>' + 'No reults found!' + '</p>');
+    } else {
+        var comingBuses = response['bustime-response'].prd.length;
+        console.log('There are ' + comingBuses + ' upcoming buses! They are:')
+        for (var i = 0; i < comingBuses; i++) {
+            console.log("Bus " + response['bustime-response'].prd[i].rt + " will arrive in " + response['bustime-response'].prd[i].prdctdn + " minutes");
+            var result = "Bus " + response['bustime-response'].prd[i].rt + " will arrive in " + response['bustime-response'].prd[i].prdctdn + " minutes";
+            $('#results').append('<p>' + result + '</p>');
+            //console.log('Concert ' + (i + 1) + ' is on ' + moment(response.data[i].datetime).format('MMMM Do YYYY') + ' at the ' + response.data[i].venue.name + ' in ' + response.data[i].venue.city + ', ' + response.data[i].venue.country);
+        }
+    }
+}
 
 //67,67,"Jackson & Canal","Jackson & Canal, Eastbound, Southwest Corner",41.877997,-87.639776,0,,1
 //1417,1417,"LaSalle & Oak","LaSalle & Oak, Southbound, Southwest Corner",41.900327,-87.632942,0,,1
